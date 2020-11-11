5dc3596c443865984510bfb361e3d5176c7f929e
miguel raz <miguelraz@gmail.com>
2020-11-10 22:46:11 -0600

    Let'sssssssss goooooo!

--- src/SymbolicTensors2.jl
+++ src/SymbolicTensors2.jl
@@ -1,5 +1,33 @@
 module SymbolicTensors2
+using LinearAlgebra
+using SymbolicUtils
+using SymbolicUtils: istree, operation, arguments, similarterm
 
-# Write your package code here.
+abstract type AbstractNVector end
+abstract type AbstractTensor <: AbstractNVector end
+struct NVector <: AbstractNVector
+    nvec::Array
+    idxs::Tuple{Int}
+    metric::Array
 
+end
+
+abstract type AbstractContraction end
+#struct Contraction <: AbstractContraction
+    #A::NVector
+    #B::NVector
+#end
+
+struct Contraction <: AbstractContraction
+    ex :: Expr
+end
+
+SymbolicUtils.istree(x::Contraction) = x.ex.head == :call
+SymbolicUtils.operation(x::Contraction) = x.ex.args[1]
+SymbolicUtils.arguments(x::Contraction) = x.ex.args[2:end]
+SymbolicUtils.symtype(x::Contraction) = Real # ?
+SymbolicUtils.similarterm(x::Contraction, f, xs) = Contraction(:(I * I))
+
+
+export Contraction
 end
